47d21e30dc5769b43517ea2f3158db026dc2dd41
Elia Oggian <elia.oggian@cscs.ch>
2022-11-08 18:15:13 +0100

    Added cert-manager examples

---
+++ cert-manager/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+helm repo add jetstack https://charts.jetstack.io
+helm repo add cert-manager-webhook-pdns https://zachomedia.github.io/cert-manager-webhook-pdns
+
+
+helm install --namespace cert-manager --version v1.10.0 -f values.yaml cert-manager jetstack/cert-manager
+helm install -n cert-manager cert-manager-webhook-pdns cert-manager-webhook-pdns/cert-manager-webhook-pdns
