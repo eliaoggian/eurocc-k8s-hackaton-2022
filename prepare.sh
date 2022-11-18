2b9e5bba29319358805971f82efac482d1b7412c
Elia Oggian <elia.oggian@cscs.ch>
2022-11-18 13:58:40 +0100

    Fixed NS IPs

--- prepare.sh
+++ prepare.sh
@@ -8,11 +8,13 @@ echo 'Please enter your cluster name (e.g "thanos" or "loki" or "hela", without 
 read cluster
 
 echo "Replacing \"test.hack2022.ch\" with \"$cluster.hack2022.ch\" in all files now."
-sed -i -r "s/test.hack2022.ch/$cluster.hack2022.ch/g" $SCRIPT_DIRECTORY
+find $SCRIPT_DIRECTORY \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/test\.hack2022\.ch/$cluster.hack2022.ch/g'
+# sed -i -r "s/test.hack2022.ch/$cluster.hack2022.ch/g" $SCRIPT_DIRECTORY
 
 echo 'Please enter your cluster VIP range (in the format "148.187.83.XXX-148.187.83.YYY", without quotes!)'
 read viprange
 
-sed -i -r "s/@@ADDRESS_POOL@@/$viprange/g" $SCRIPT_DIRECTORY
+# sed -i -r "s/@@ADDRESS_POOL@@/$viprange/g" $SCRIPT_DIRECTORY
+find $SCRIPT_DIRECTORY \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/@@ADDRESS_POOL@@/$viprange/g"
 
 echo "All done! The files are ready for the cluster: $cluster" 
